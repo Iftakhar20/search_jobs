94355044d5400266d9a599d4f79b6a92e92d1dce
Iftekhar Ali <dogzrgood2@gmail.com>
2015-05-22 13:42:25 +0400

    Updated search_jobs.py

---
+++ search_jobs.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Master
+#
+# Created:     17/05/2015
+# Copyright:   (c) Master 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+from bs4 import BeautifulSoup
+from urllib import request, urlopen
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
