909a9ccee988f8855ff2314fc7fb0c06b79efa51
Iftekhar Ali <dogzrgood2@gmail.com>
2015-05-23 19:12:40 +0400

    v.25 flask app

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, redirect
+import search_jobs
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return 'hi there'
+
+@app.route('/job_search')
+def search_jobs_():
+    search_jobs.main()
+
+@app.route('/view_jobs')
+def view_jobs():
+    return redirect("static/results.txt")
 
 if __name__== '__main__':
     app.run()
